c0a4997f67e107d9dc67a70f6d677d6beaf4f2d1
Justin Kambic <justin@Justins-MacBook-Pro.local>
2017-06-01 11:53:00 -0400

    Initial Commit

---
+++ free-blur/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  free-blur
+//
+//  Created by Justin Kambic on 6/1/17.
+//  Copyright © 2017 Justin Kambic. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ free-blur/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  free-blur
+//
+//  Created by Justin Kambic on 6/1/17.
+//  Copyright © 2017 Justin Kambic. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
