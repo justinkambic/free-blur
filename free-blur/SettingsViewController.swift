245a2fed235ee244ff1e09a05ee090c2134f21ff
Justin Kambic <justin@Justins-MacBook-Pro.local>
2017-07-26 22:12:11 -0400

    Changed name of settings view controller.

--- free-blur/SettingsViewController.swift
+++ free-blur/SettingsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SettingsViewController: UIViewController {
     let blurSettings = BlurSettings()
     
     @IBOutlet weak var diamval: UILabel!
